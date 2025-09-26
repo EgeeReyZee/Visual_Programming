b27861b6d8ff633ff557667bf1ad9a6df53de14d
EgeeReyZee <valekzhmikh@gmail.com>
2025-09-26 11:04:58 +0700

    Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,6 @@
-import kotlin.random.Random
-import kotlin.math.sqrt
 import kotlin.math.pow
-import java.util.Timer
-import java.util.TimerTask
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 open class Human {
     var name: String = ""
@@ -35,45 +33,46 @@ open class Human {
         y = _toY
         println("$name пошел на позицию: (${"%.2f".format(x)}; ${"%.2f".format(y)})")
     }
-    open fun move(_sec: Int) {
-        for (n in 1 .. _sec) {
+    open fun move() {
+        Thread {
             val curX = x
             val curY = y
 
-            val movedX = Random.nextDouble(speed)
+            val movedX = Random.Default.nextDouble(speed)
             val movedY = sqrt(speed.pow(2) - movedX.pow(2))
-            if (Random.nextBoolean() == true) {
+            if (Random.Default.nextBoolean() == true) {
                 x += movedX
             } else {
                 x -= movedX
             }
-            if (Random.nextBoolean() == true) {
+            if (Random.Default.nextBoolean() == true) {
                 y += movedY
             } else {
                 y -= movedY
             }
-        }
+        }.start()
     }
 }
+
 class Driver(name: String, surname: String, second_name: String, age: Int, speed: Double, val car_model: String) :
     Human(name, surname,  second_name, age, speed) {
-    override fun move(_sec: Int) {
+    override fun move() {
         Thread {
 
-            if (Random.nextBoolean() == true) {
-                if (Random.nextBoolean() == true) {
+            if (Random.Default.nextBoolean() == true) {
+                if (Random.Default.nextBoolean() == true) {
                     x += speed
                 } else {
                     x -= speed
                 }
             } else {
-                if (Random.nextBoolean() == true) {
+                if (Random.Default.nextBoolean() == true) {
                     y += speed
                 } else {
                     y -= speed
                 }
             }
-        }
+        }.start()
     }
 }
 
@@ -85,12 +84,13 @@ fun main() {
     )
 
     val Vasya: Driver = Driver("Василий", "Абрамович", "Абрамович", 28, 23.5, "Alfa Romeo")
-
-    for (human in humans) {
-        human.move(45)
-        human.position()
+    while (true) {
+        for (human in humans) {
+            human.move()
+            human.position()
+        }
+        Vasya.move()
+        Vasya.position()
+        Thread.sleep(1000)
     }
-    Vasya.move(45)
-    Vasya.position()
-    Thread.sleep(1000)
 }
