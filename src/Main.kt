afbc81bac14a127b65e5e8f3347b386ac387170b
EgeeReyZee <valekzhmikh@gmail.com>
2025-09-26 10:57:50 +0700

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.system.exitProcess
+
 fun main() {
     val humans = arrayOf(
         Human("Иван", "Иванов", "Иванович", 25, 8.5),
@@ -6,12 +8,13 @@ fun main() {
     )
 
     val Vasya: Driver = Driver("Василий", "Абрамович", "Абрамович", 28, 23.5, "Alfa Romeo")
-
-    for (human in humans) {
-        human.move(45)
-        human.position()
+    while (true) {
+        for (human in humans) {
+            human.move()
+            human.position()
+        }
+        Vasya.move()
+        Vasya.position()
+        Thread.sleep(1000)
     }
-    Vasya.move(45)
-    Vasya.position()
-    Thread.sleep(1000)
 }
